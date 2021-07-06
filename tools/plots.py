3e3f79faa520f5e15beebbf323bc4b7b5da2d765
Yaopeng-PC <mailmyps@gmail.com>
2021-07-06 16:43:19 +0300

    Add plot tools

---
+++ tools/plots.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+from math import ceil
+
+
+def plot_one_sample(X, Y, Y_pred=None, ax=None):
+    assert ax is not None
+    ax.imshow(X.view(20, 20).T, cmap="Greys")
+    ax.set_xticks([])
+    ax.set_yticks([])
+    if Y_pred is None:
+        ax.set_title(f"{int(Y)}")
+    else:
+        ax.set_title(f"True: {int(Y)}, Predicted: {int(Y_pred)}")
+
+
+def plot_samples(X, Y, Y_pred=None, nrows=1, figsize=(8, 6)):
+    images_shape = X.size()
+    if len(images_shape) == 2:
+        total_image = images_shape[0]
+    else:
+        total_image = 1
+    ncolumes = ceil(total_image / nrows)
+    fig, axes = plt.subplots(nrows, ncolumes, figsize=figsize)
+    for i in range(nrows):
+        for j in range(ncolumes):
+            if nrows == ncolumes == 1:
+                ax = axes
+            elif nrows == 1 or ncolumes == 1:
+                ax = axes[max(i, j)]
+            else:
+                ax = axes[i, j]
+            index = i * nrows + j
+            if index >= total_image:
+                ax.axis('off')
+                continue
+            if Y_pred is None:
+                plot_one_sample(X[index], Y[index] % 10, ax=ax)
+            else:
+                plot_one_sample(X[index], Y[index] % 10, Y_pred[index], ax=ax)
+    return fig, axes
